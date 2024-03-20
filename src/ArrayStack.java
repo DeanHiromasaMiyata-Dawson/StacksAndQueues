7a383db0ab5c216213e434b90a39e6f00085b723
Dean Hiromasa Miyata-Dawson <51884979+DeanHiromasaMiyata-Dawson@users.noreply.github.com>
2024-03-19 21:02:06 -0400

    Update ArrayStack.java

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -1,14 +1,5 @@
 /**
- * Your implementation of an ArrayStack.
- *
- * @author Dean Hiromasa Miyata-Dawson
- * @version 1.0
- * @userid ddawson42
- * @GTID 903833148
- *
- * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
- *
- * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ * Implementation of an ArrayStack.
  */
 
 import java.util.NoSuchElementException;
@@ -17,14 +8,8 @@ public class ArrayStack<T> {
 
     /*
      * The initial capacity of the ArrayStack.
-     *
-     * DO NOT MODIFY THIS VARIABLE.
      */
     public static final int INITIAL_CAPACITY = 9;
-
-    /*
-     * Do not add new instance variables or modify existing ones.
-     */
     private T[] backingArray;
     private int size;
 
@@ -38,11 +23,10 @@ public class ArrayStack<T> {
 
     /**
      * Adds the data to the top of the stack.
-     *
      * If sufficient space is not available in the backing array, resize it to
      * double the current length.
      *
-     * Must be amortized O(1).
+     * Amortized O(1).
      *
      * @param data the data to add to the top of the stack
      * @throws java.lang.IllegalArgumentException if data is null
@@ -63,13 +47,10 @@ public class ArrayStack<T> {
     }
 
     /**
-     * Removes and returns the data from the top of the stack.
+     * Removes and returns the data from the top of the stack without shrinking the backing array.
+     * Replace any spots that are poped from with null.
      *
-     * Do not shrink the backing array.
-     *
-     * Replace any spots that you pop from with null.
-     *
-     * Must be O(1).
+     * O(1).
      *
      * @return the data formerly located at the top of the stack
      * @throws java.util.NoSuchElementException if the stack is empty
@@ -87,7 +68,7 @@ public class ArrayStack<T> {
     /**
      * Returns the data from the top of the stack without removing it.
      *
-     * Must be O(1).
+     * O(1).
      *
      * @return the data from the top of the stack
      * @throws java.util.NoSuchElementException if the stack is empty
@@ -102,26 +83,18 @@ public class ArrayStack<T> {
     /**
      * Returns the backing array of the stack.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the backing array of the stack
      */
     public T[] getBackingArray() {
-        // DO NOT MODIFY THIS METHOD!
         return backingArray;
     }
 
     /**
      * Returns the size of the stack.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the size of the stack
      */
     public int size() {
-        // DO NOT MODIFY THIS METHOD!
         return size;
     }
 }
