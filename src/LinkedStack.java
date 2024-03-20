75a41807c48a647b6aede13510db87e19e3b735d
Dean Hiromasa Miyata-Dawson <51884979+DeanHiromasaMiyata-Dawson@users.noreply.github.com>
2024-03-19 21:05:16 -0400

    Update LinkedStack.java

--- src/LinkedStack.java
+++ src/LinkedStack.java
@@ -1,34 +1,17 @@
 /**
- * Your implementation of a LinkedStack. It should NOT be circular.
- *
- * @author Dean Hiromasa Miyata-Dawson
- * @version 1.0
- * @userid ddawson42
- * @GTID 903833148
- *
- * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
- *
- * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ * Non-circular implementation of a LinkedStack. It should NOT be circular.
  */
 
 import java.util.NoSuchElementException;
 
 public class LinkedStack<T> {
-
-    /*
-     * Do not add new instance variables or modify existing ones.
-     */
     private LinkedNode<T> head;
     private int size;
-
-    /*
-     * Do not add a constructor.
-     */
 
     /**
      * Adds the data to the top of the stack.
      *
-     * Must be O(1).
+     * O(1).
      *
      * @param data the data to add to the top of the stack
      * @throws java.lang.IllegalArgumentException if data is null
@@ -46,7 +29,7 @@ public class LinkedStack<T> {
     /**
      * Removes and returns the data from the top of the stack.
      *
-     * Must be O(1).
+     * O(1).
      *
      * @return the data formerly located at the top of the stack
      * @throws java.util.NoSuchElementException if the stack is empty
@@ -65,7 +48,7 @@ public class LinkedStack<T> {
     /**
      * Returns the data from the top of the stack without removing it.
      *
-     * Must be O(1).
+     * O(1).
      *
      * @return the data from the top of the stack
      * @throws java.util.NoSuchElementException if the stack is empty
@@ -81,26 +64,18 @@ public class LinkedStack<T> {
     /**
      * Returns the head node of the stack.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the node at the head of the stack
      */
     public LinkedNode<T> getHead() {
-        // DO NOT MODIFY!
         return head;
     }
 
     /**
      * Returns the size of the stack.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the size of the stack
      */
     public int size() {
-        // DO NOT MODIFY THIS METHOD!
         return size;
     }
 }
