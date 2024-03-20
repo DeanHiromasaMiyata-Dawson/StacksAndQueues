0949342bc897a5f7b9f361a0a90f7dfdf2c2e5c6
Dean Hiromasa Miyata-Dawson <51884979+DeanHiromasaMiyata-Dawson@users.noreply.github.com>
2024-03-19 21:04:26 -0400

    Update LinkedQueue.java

--- src/LinkedQueue.java
+++ src/LinkedQueue.java
@@ -1,35 +1,18 @@
 /**
- * Your implementation of a LinkedQueue. It should NOT be circular.
- *
- * @author Dean Hiromasa Miyata-Dawson
- * @version 1.0
- * @userid ddawson42
- * @GTID 903833148
- *
- * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
- *
- * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ * Non-circular implementation of a LinkedQueue.
  */
 
 import java.util.NoSuchElementException;
 
 public class LinkedQueue<T> {
-
-    /*
-     * Do not add new instance variables or modify existing ones.
-     */
     private LinkedNode<T> head;
     private LinkedNode<T> tail;
     private int size;
 
-    /*
-     * Do not add a constructor.
-     */
-
     /**
      * Adds the data to the back of the queue.
      *
-     * Must be O(1).
+     * O(1).
      *
      * @param data the data to add to the back of the queue
      * @throws java.lang.IllegalArgumentException if data is null
@@ -52,7 +35,7 @@ public class LinkedQueue<T> {
     /**
      * Removes and returns the data from the front of the queue.
      *
-     * Must be O(1).
+     * O(1).
      *
      * @return the data formerly located at the front of the queue
      * @throws java.util.NoSuchElementException if the queue is empty
@@ -73,7 +56,7 @@ public class LinkedQueue<T> {
     /**
      * Returns the data from the front of the queue without removing it.
      *
-     * Must be O(1).
+     * O(1).
      *
      * @return the data located at the front of the queue
      * @throws java.util.NoSuchElementException if the queue is empty
@@ -89,39 +72,27 @@ public class LinkedQueue<T> {
     /**
      * Returns the head node of the queue.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the node at the head of the queue
      */
     public LinkedNode<T> getHead() {
-        // DO NOT MODIFY THIS METHOD!
         return head;
     }
 
     /**
      * Returns the tail node of the queue.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the node at the tail of the queue
      */
     public LinkedNode<T> getTail() {
-        // DO NOT MODIFY THIS METHOD!
         return tail;
     }
 
     /**
      * Returns the size of the queue.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the size of the queue
      */
     public int size() {
-        // DO NOT MODIFY THIS METHOD!
         return size;
     }
 }
