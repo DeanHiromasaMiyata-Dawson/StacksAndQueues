1f7f40b7fdeed16695b1c3f464526f8b0cb1b12a
Dean Hiromasa Miyata-Dawson <51884979+DeanHiromasaMiyata-Dawson@users.noreply.github.com>
2024-03-19 20:55:33 -0400

    Add files via upload

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -1,2 +1,127 @@
-package PACKAGE_NAME;public class ArrayStack {
+/**
+ * Your implementation of an ArrayStack.
+ *
+ * @author Dean Hiromasa Miyata-Dawson
+ * @version 1.0
+ * @userid ddawson42
+ * @GTID 903833148
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+
+import java.util.NoSuchElementException;
+
+public class ArrayStack<T> {
+
+    /*
+     * The initial capacity of the ArrayStack.
+     *
+     * DO NOT MODIFY THIS VARIABLE.
+     */
+    public static final int INITIAL_CAPACITY = 9;
+
+    /*
+     * Do not add new instance variables or modify existing ones.
+     */
+    private T[] backingArray;
+    private int size;
+
+    /**
+     * Constructs a new ArrayStack.
+     */
+    public ArrayStack() {
+        backingArray = (T[]) new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    /**
+     * Adds the data to the top of the stack.
+     *
+     * If sufficient space is not available in the backing array, resize it to
+     * double the current length.
+     *
+     * Must be amortized O(1).
+     *
+     * @param data the data to add to the top of the stack
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void push(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Error: data is empty.");
+        }
+
+        if (size == backingArray.length) {
+            T[] tempArray = (T[]) new Object[backingArray.length * 2];
+            for (int i = 0; i < backingArray.length; i++) {
+                tempArray[i] = backingArray[i];
+            }
+            backingArray = tempArray;
+        }
+        backingArray[size++] = data;
+    }
+
+    /**
+     * Removes and returns the data from the top of the stack.
+     *
+     * Do not shrink the backing array.
+     *
+     * Replace any spots that you pop from with null.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the top of the stack
+     * @throws java.util.NoSuchElementException if the stack is empty
+     */
+    public T pop() {
+        if (size == 0) {
+            throw new NoSuchElementException("Error: stack is empty.");
+        }
+        T temp = peek();
+        size--;
+        backingArray[size] = null;
+        return temp;
+    }
+
+    /**
+     * Returns the data from the top of the stack without removing it.
+     *
+     * Must be O(1).
+     *
+     * @return the data from the top of the stack
+     * @throws java.util.NoSuchElementException if the stack is empty
+     */
+    public T peek() {
+        if (size == 0) {
+            throw new NoSuchElementException("Error: stack is empty.");
+        }
+        return backingArray[size - 1];
+    }
+
+    /**
+     * Returns the backing array of the stack.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the backing array of the stack
+     */
+    public T[] getBackingArray() {
+        // DO NOT MODIFY THIS METHOD!
+        return backingArray;
+    }
+
+    /**
+     * Returns the size of the stack.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the stack
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
 }
--- src/LinkedQueue.java
+++ src/LinkedQueue.java
@@ -1,2 +1,127 @@
-package PACKAGE_NAME;public class LinkedQueue {
+/**
+ * Your implementation of a LinkedQueue. It should NOT be circular.
+ *
+ * @author Dean Hiromasa Miyata-Dawson
+ * @version 1.0
+ * @userid ddawson42
+ * @GTID 903833148
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+
+import java.util.NoSuchElementException;
+
+public class LinkedQueue<T> {
+
+    /*
+     * Do not add new instance variables or modify existing ones.
+     */
+    private LinkedNode<T> head;
+    private LinkedNode<T> tail;
+    private int size;
+
+    /*
+     * Do not add a constructor.
+     */
+
+    /**
+     * Adds the data to the back of the queue.
+     *
+     * Must be O(1).
+     *
+     * @param data the data to add to the back of the queue
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void enqueue(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Error: data is empty.");
+        }
+
+        LinkedNode<T> temp = new LinkedNode<>(data);
+        if (size == 0) {
+            head = temp;
+        } else {
+            tail.setNext(temp);
+        }
+        tail = temp;
+        size++;
+    }
+
+    /**
+     * Removes and returns the data from the front of the queue.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the front of the queue
+     * @throws java.util.NoSuchElementException if the queue is empty
+     */
+    public T dequeue() {
+        if (size == 0) {
+            throw new NoSuchElementException("Error: queue is empty.");
+        }
+
+        T temp = head.getData();
+        head = head.getNext();
+        if (size-- == 0) {
+            tail = null;
+        }
+        return temp;
+    }
+
+    /**
+     * Returns the data from the front of the queue without removing it.
+     *
+     * Must be O(1).
+     *
+     * @return the data located at the front of the queue
+     * @throws java.util.NoSuchElementException if the queue is empty
+     */
+    public T peek() {
+        if (size == 0) {
+            throw new NoSuchElementException("Error: queue is empty.");
+        }
+
+        return head.getData();
+    }
+
+    /**
+     * Returns the head node of the queue.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the node at the head of the queue
+     */
+    public LinkedNode<T> getHead() {
+        // DO NOT MODIFY THIS METHOD!
+        return head;
+    }
+
+    /**
+     * Returns the tail node of the queue.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the node at the tail of the queue
+     */
+    public LinkedNode<T> getTail() {
+        // DO NOT MODIFY THIS METHOD!
+        return tail;
+    }
+
+    /**
+     * Returns the size of the queue.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the queue
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
 }
